6fabd9e267f6c236a81c992a57194eb3b3f05442
Daniel Iturralde <daniel_iturralde_b@hotmail.com>
2023-12-06 18:47:50 -0500

    Extract the localStorage instruction into a new Hook outside my main app file, after this procede to manage all my files into new folders

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { TodoCounter } from './TodoCounter';
-import { TodoSearch } from './TodoSearch';
-import { TodoList } from './TodoList';
-import { TodoItem } from './TodoItem';
-import { CreateTodoBotton } from './CreateTodoButton';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { CreateTodoBotton } from '../CreateTodoButton';
+import { useLocalStorage } from './useLocalStorage'
 
 // const defaultTodos = [
 //   {text: 'Cortar cebollas', completed: true},
@@ -18,19 +19,8 @@ import { CreateTodoBotton } from './CreateTodoButton';
 
 function App() {
 
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
-
-  let parsedTodos
-
-  if (!localStorageTodos) {
-    localStorage.setItem('TODOS_V1',JSON.stringify([]));
-    parsedTodos = [];
-  } else {
-    parsedTodos = JSON.parse(localStorageTodos);
-  }
-
   // setting states
-  const [todos, setTodos] = React.useState(parsedTodos);
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = React.useState('');
 
   // setting derived states
@@ -50,14 +40,6 @@ function App() {
 
     }
   );
-
-  const saveTodos = (newTodos) =>  {
-    
-    localStorage.setItem('TODOS_V1', JSON.stringify(newTodos));
-
-    setTodos(newTodos);
-
-  }
   
   const completeTodo = (text) => {
 
