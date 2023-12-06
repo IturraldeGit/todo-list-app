c75786a0c1dbc26ac82c609682f83d75aac34ea6
Daniel Iturralde <daniel_iturralde_b@hotmail.com>
2023-12-06 03:20:43 -0500

    Adding new svg icons with the onClick logig and also it is introduced the localStorage dynamic with the delete and complete button

--- src/App.js
+++ src/App.js
@@ -5,18 +5,32 @@ import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoBotton } from './CreateTodoButton';
 
-const defaultTodos = [
-  {text: 'Cortar cebollas', completed: true},
-  {text: 'Tomar el curso de Introducción a React.js', completed: false},
-  {text: 'Llorar con la Llorona', completed: false},
-  {text: 'Lalalala', completed: false},
-  {text: 'Usar estados derivados', completed: true},
-];
+// const defaultTodos = [
+//   {text: 'Cortar cebollas', completed: true},
+//   {text: 'Tomar el curso de Introducción a React.js', completed: false},
+//   {text: 'Llorar con la Llorona', completed: false},
+//   {text: 'Lalalala', completed: false},
+//   {text: 'Usar estados derivados', completed: true},
+// ];
+
+// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
+// localStorage.removeItem('TODOS_V1');
 
 function App() {
 
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+
+  let parsedTodos
+
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1',JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
   // setting states
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
 
   // setting derived states
@@ -36,17 +50,25 @@ function App() {
 
     }
   );
+
+  const saveTodos = (newTodos) =>  {
+    
+    localStorage.setItem('TODOS_V1', JSON.stringify(newTodos));
+
+    setTodos(newTodos);
+
+  }
   
   const completeTodo = (text) => {
 
     const newTodos = [...todos];
 
     const todoIndex = newTodos.findIndex(
-      (todo) => todo.text == text
+      (todo) => todo.text === text
     );
     newTodos[todoIndex].completed = true;
     
-    setTodos(newTodos);
+    saveTodos(newTodos);
 
   }
 
@@ -55,11 +77,11 @@ function App() {
     const newTodos = [...todos];
 
     const todoIndex = newTodos.findIndex(
-      (todo) => todo.text == text
+      (todo) => todo.text === text
     );
     newTodos.splice(todoIndex, 1);
     
-    setTodos(newTodos);
+    saveTodos(newTodos);
 
   }
    
